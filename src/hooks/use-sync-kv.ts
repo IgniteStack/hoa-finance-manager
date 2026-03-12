1652014b2118c266431a78b6aea296f639bde2ab
luistiradomx <ferchop39@gmail.com>
2026-03-12 22:45:48 +0000

    Edited Spark

--- src/hooks/use-sync-kv.ts
+++ src/hooks/use-sync-kv.ts
@@ -1,20 +1,20 @@
 import { useEffect, useCallback } from 'react'
-import { useKV } from '@github/spark/hooks'
+
 
 export function useSyncKV<T>(key: string, defaultValue: T, pollInterval = 2000) {
   const [value, setValue, deleteValue] = useKV<T>(key, defaultValue)
 
-  const forceRefresh = useCallback(async () => {
-    const freshValue = await window.spark.kv.get<T>(key)
-    if (freshValue !== undefined) {
       setValue(freshValue)
-    }
   }, [key, setValue])
-
   useEffect(() => {
-    const interval = setInterval(forceRefresh, pollInterval)
-    return () => clearInterval(interval)
-  }, [forceRefresh, pollInterval])
+    return () => clearInte
 
-  return [value, setValue, deleteValue, forceRefresh] as const
 }
+
+
+
+
+
+
+
+
